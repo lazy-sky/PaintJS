9bcab63c17cc99ca4bfbd1dd2de6d0daf92460e4
lazy-sky <47808461+lazy-sky@users.noreply.github.com>
2020-02-25 16:32:46 +0900

    adding painting, filling, to save.

---
+++ app.js
@@ -0,0 +1,112 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  console.log("creating path in ", x, y);
+  if(!painting) {
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+  } else {
+      ctx.lineTo(x, y);
+      ctx.stroke();
+      console.log("creating line in ", x, y);
+  }
+}
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeChange(event) {
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+function handleCanvasClick(event) {
+    if(filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    } 
+}
+
+function handleCM(event) {
+    // 원래는 우클릭으로도 이미지를 저장할 수 있지만 유저가 
+    // 'SAVE' 버튼을 이용하길 바라기 때문에 우클릭은 막아 놓았다.
+    event.preventDefault();
+}
+
+function handleSaveClick(event) {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[EXPORT]";
+    link.click();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorClick)
+);
+
+
+
+if(range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode) {
+    mode.addEventListener("click", handleModeChange);
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
+}
